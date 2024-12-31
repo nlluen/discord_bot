fc09429c6c831a3d50eedfd69dce1f16e96ccd12
Nick Lluen <nlluen24@gmail.com>
2024-12-31 18:25:21 -0500

    Updated add_role and remove_roles to send messages so only the interaction user could to avoid spam and clutter. New create_announcement command for users to queue up announcements that mods can approve of. Also a way to check all announcements using the announcements command.

--- modules/Utility/cog.py
+++ modules/Utility/cog.py
@@ -1,9 +1,11 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
-from config import guild_id
+from config import guild_id, announcement_channel_id
 import asyncio
 import random
+import pytz
+import json
 
 
 class Utility(commands.Cog):
@@ -37,8 +39,6 @@ class Utility(commands.Cog):
 
     @app_commands.command(name='flip', description='Flip a coin to make your basic life choices')
     @app_commands.guilds(guild_id)
-    # @commands.cooldown(1, 1, commands.BucketType.user)
-    # @commands.command(name='flip', help='Flip a coin to make your basic life choices and guess the outcome')
     async def flip(self, interaction: discord.Interaction, guess: str=None):
         HoT = random.randint(0, 1)
 
@@ -53,42 +53,50 @@ class Utility(commands.Cog):
                 await interaction.response.send_message(f"You were correct! It was {side}")
             else:
                 await interaction.response.send_message(f"You were wrong! It was {side}")
+            return
 
         if HoT:
             await interaction.response.send_message("Heads!")
         else:
             await interaction.response.send_message("Tails!")
 
-    @app_commands.command(name='addrole', description='Give yourself a role')
+    @app_commands.command(name='add_role', description='Give yourself a role')
     @app_commands.guilds(guild_id)
-    # @commands.cooldown(1, 1, commands.BucketType.user)
-    # @commands.command(name='flip', help='Flip a coin to make your basic life choices and guess the outcome')
-    async def addrole(self, interaction: discord.Interaction, role: discord.Role):
+    async def add_role(self, interaction: discord.Interaction, role: discord.Role):
         roles = interaction.user.roles
         restricted_roles = ["Men", "Untitled", "Champion of Halloween", "The Godfather", "The Godmother", "The Godson", "ECE", "Hall Of Shame"]
         if role not in roles and role.name not in restricted_roles:
             await interaction.user.add_roles(role)
-            await interaction.response.send_message(f"You have been given the *{role}* role")
+            await interaction.response.send_message(f"You have been given the *{role}* role", ephemeral=True)
         elif role in roles:
-            await interaction.response.send_message("You already have this role!")
+            await interaction.response.send_message("You already have this role!", ephemeral=True)
         else:
-            await interaction.response.send_message("Sorry but you cannot access that role!")
+            await interaction.response.send_message("Sorry but you cannot access that role!", ephemeral=True)
 
-    @app_commands.command(name='removerole', description='Remove a role')
+    @app_commands.command(name='remove_role', description='Remove a role')
     @app_commands.guilds(guild_id)
-    # @commands.cooldown(1, 1, commands.BucketType.user)
-    # @commands.command(name='flip', help='Flip a coin to make your basic life choices and guess the outcome')
-    async def removerole(self, interaction: discord.Interaction, role: discord.Role):
+    async def remove_role(self, interaction: discord.Interaction, role: discord.Role):
         roles = interaction.user.roles
         if role in roles:
             await interaction.user.remove_roles(role)
-            await interaction.response.send_message(f"Successfully removed the {role} role!")
+            await interaction.response.send_message(f"Successfully removed the {role} role!", ephemeral=True)
         else:
-            await interaction.response.send_message("You cannot remove a role you do not have!")
+            await interaction.response.send_message("You cannot remove a role you do not have!", ephemeral=True)
+
+    @app_commands.command(name="roleinfo", description="Get the information about a role")
+    @app_commands.guilds(guild_id)
+    async def role_info(self, interaction: discord.Interaction, role: discord.Role):
+        members_with_role = [member for member in role.members]
+        member_names = " • ".join([member.display_name for member in members_with_role])
+
+        embed = discord.Embed(title=f"Role Info: {role.name}", color=discord.Color.blue())
+        embed.add_field(name="Members with this role", value=len(members_with_role), inline=False)
+        embed.add_field(name="Members", value=member_names, inline=False)
+        await interaction.response.send_message(embed=embed)
 
     @app_commands.command(name='serverinfo', description='Get the general info for the server!')
     @app_commands.guilds(guild_id)
-    async def serverinfo(self, interaction: discord.Interaction):
+    async def server_info(self, interaction: discord.Interaction):
         guild = interaction.guild
         guild_name = guild.name
         guild_description = guild.description
@@ -107,43 +115,52 @@ class Utility(commands.Cog):
         embed.add_field(name="Voice Channels", value=guild_voice_channel_count)
         await interaction.response.send_message(embed=embed)
 
-    @app_commands.command(name="roleinfo", description="Get the information about a role")
+    
+
+    @app_commands.command(name="create_announcement", description="Queue your announcement for a mod to approve")
     @app_commands.guilds(guild_id)
-    async def roleinfo(self, interaction: discord.Interaction, role: discord.Role):
-        members_with_role = [member for member in role.members]
-        member_names = " • ".join([member.display_name for member in members_with_role])
+    async def create_announcement(self, interaction: discord.Interaction, brief_description: str, message: str):
 
-        embed = discord.Embed(title=f"Role Info: {role.name}", color=discord.Color.blue())
-        embed.add_field(name="Members with this role", value=len(members_with_role), inline=False)
-        embed.add_field(name="Members", value=member_names, inline=False)
+        #print(interaction.created_at.astimezone(pytz.timezone('US/Eastern')).strftime('%m/%d/%Y at %H:%M:%S'))
+        dic = {
+            "Description": brief_description,
+            "Message": message,
+            "Announcer": interaction.user.id,
+            "Time_Created": interaction.created_at.astimezone(pytz.timezone('US/Eastern')).strftime('%m/%d/%Y at %I:%M %p')
+        }
+
+        with open('announcements.json', 'r') as rf:
+            announcements = json.load(rf)
+
+        if len(announcements) == 5:
+            await interaction.response.send_message("Too many announcements queued, ask the mods to clear them.")
+            return
+        
+        announcements.append(dic)
+
+        with open('announcements.json', 'w') as wf:
+            json.dump(announcements, wf, indent = 4 )
+
+        await interaction.response.send_message("Thank you for your announcement, a mod will review and approve it shortly.")
+        
+    @app_commands.command(name='announcements', description="View all queued announcements")
+    @app_commands.guilds(guild_id)
+    async def announcements(self, interaction: discord.Interaction):
+        embed = discord.Embed(title="Announcements Queued", color=discord.Color.blue())
+        
+        with open("announcements.json", 'r') as rf:
+            announcements = json.load(rf)
+
+        embed.description = ""
+        for i, announcement in enumerate(announcements):
+            # print(announcement)
+            announcer = interaction.guild.get_member(announcement['Announcer'])
+            embed.description += f"{i + 1}: " + f"{announcement['Description']} - {announcer.display_name}" + "\n"
+            #embed.add_field(name= f"{i+1}", value= f"{announcement['Description']}", inline=True)
+            #embed.add_field(name="Time Queued", value=f"{announcement['Time_Created']}", inline=True)
+            
+
         await interaction.response.send_message(embed=embed)
-
-    #
-    # @app_commands.command(name='flip2', description='Flip a coin and bet on which side it lands')
-    # @app_commands.guilds(guild_id)
-    # async def flip2(self, interaction: discord.Interaction):
-    #     await interaction.response.send_message('Coin is flipping in the air! Call it!')
-    #     time.sleep(3.0)
-    #
-    #     def check(m):
-    #         return (m.content.lower() == 'heads' or m.content.lower() == 'tails') and m.author == ctx.author
-    #     try:
-    #         message = await self.bot.wait_for('message', timeout=0.5, check=check)
-    #     except asyncio.TimeoutError:
-    #         await ctx.send('There are only two sides to a coin ... and that is not one of them! Try again.')
-    #     else:
-    #         print(message.content)
-    #         HoT = random.randint(0, 1)
-    #         print(HoT)
-    #         if HoT:
-    #             side = 'HEADS'
-    #         else:
-    #             side = 'TAILS'
-    #
-    #         if side == message.content.upper():
-    #             await ctx.send(f'landed on {side}! You were correct!')
-    #         else:
-    #             await ctx.send(f'landed on {side}! You were wrong!')
 
 
 async def setup(bot):
